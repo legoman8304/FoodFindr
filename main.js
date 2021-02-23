a80756be19d52f456b77fcf24b94c883499c8a98
Wyatt Nulton <legoman8304@gmail.com>
2021-02-23 12:54:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function getWeather(a){
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/', //Anti cors proxy to eliminate cors errors on api calls.
-        targetUrl = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' + 'json' + '?' + config.MY_KEY + '&' + ''
+        targetUrl = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' + 'json' + '?' + config.MY_KEY + '&' + 'food' + '&' + 
   const requestURL = proxyUrl + targetUrl
   const request = new XMLHttpRequest();
   request.open('GET', requestURL);
