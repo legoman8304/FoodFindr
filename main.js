0e01ab8a816cbddc12c4da49dd31c23b08f373e6
Wyatt Nulton <legoman8304@gmail.com>
2021-02-25 12:55:28 -0500

    fix

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-function getWeather(a){
+/* function a(a){
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/', //Anti cors proxy to eliminate cors errors on api calls.
-        targetUrl = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' + 'json' + '?' + config.MY_KEY + '&' + 'food' + '&' + 
+        targetUrl = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' + 'json' + '?' + MY_KEY + '&' + 'food' + '&' + 'textquery' 
   const requestURL = proxyUrl + targetUrl
   const request = new XMLHttpRequest();
-  request.open('GET', requestURL);
+  request.open('GET', targetUrl);
   request.responseType = 'json';
   request.send();
   request.onload = function() {
     const req = request
     console.log(req)
-    document.getElementById("current").innerHTML = req.response.currently.apparentTemperature
+    document.getElementById("log").innerHTML = req;
   }
 }
+a();
+
+Old method of fetching api, switching to jquery.
+
+*/ 
+$.ajax({
+  url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' + 'json' + '?' + MY_KEY + '&' + 'food' + '&' + 'textquery',
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  type: "GET", /* can use POST, GET, or PUT*/
+  dataType: "json",
+  data: {
+  },
+  success: function (result) {
+      console.log(result);
+      document.getElementById("log").innerHTML = result;
+  },
+  error: function () {
+      console.log("error");
+  }
+});
