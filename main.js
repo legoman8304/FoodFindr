fa4c0fb4384b8dfa17f3e30762084a612b98fb7f
Wyatt Nulton <legoman8304@gmail.com>
2021-03-09 12:55:44 -0500

    made sorting algorithm

--- main.js
+++ main.js
@@ -26,8 +26,15 @@ $.ajax({
   success: function( data ) {
     console.debug("success")
     console.log(data);
-    console.log("used - https://api.foursquare.com/v2/venues/search?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v=20180323&near=Parkersburg,WV&radius=100000&categoryId=4d4b7105d754a06374d81259")
-    //console.log(data.response.venues[0]);
+    console.debug("used - https://api.foursquare.com/v2/venues/search?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v=20180323&near=Parkersburg,WV&radius=100000&categoryId=4d4b7105d754a06374d81259")
+    var places = [data.response.venues[0].name, data.response.venues[1].name, data.response.venues[2].name, data.response.venues[3].name, data.response.venues[4].name, data.response.venues[5].name, data.response.venues[6].name, data.response.venues[7].name, data.response.venues[8].name, data.response.venues[9].name, data.response.venues[10].name, data.response.venues[11].name, data.response.venues[12].name, data.response.venues[13].name, data.response.venues[14].name, data.response.venues[15].name, data.response.venues[16].name, data.response.venues[17].name, data.response.venues[18].name, data.response.venues[19].name, data.response.venues[20].name, data.response.venues[21].name, data.response.venues[22].name, data.response.venues[23].name, data.response.venues[24].name, data.response.venues[25].name, data.response.venues[26].name, data.response.venues[27].name, data.response.venues[28].name, data.response.venues[29].name, ];
+    //var places = [data.response.venues[6].name, data.response.venues[10].name, data.response.venues[11].name, data.response.venues[13].name,];
+    var uniquePlaces = [];
+    $.each(places, function(i, el){
+    if($.inArray(el, uniquePlaces) === -1) uniquePlaces.push(el);
+    })
+    console.log(places);
+    console.log(uniquePlaces);
     for (let index = 0; index < 29; index++) {
       document.getElementById(index).innerHTML = data.response.venues[index].name + " at " + data.response.venues[index].location.address
     }
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import http.server
 import socketserver
 
-PORT = 7000
+PORT = 9000
 
 Handler = http.server.SimpleHTTPRequestHandler
 
