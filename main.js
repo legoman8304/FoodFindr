48a4632f1ea405091c1b6c6ba4ba97a97c0e73d2
Wyatt Nulton <legoman8304@gmail.com>
2023-01-24 23:53:16 -0500

    Crisis averted
    Disregard the anger from last commit. I fixed it in a matter of minutes. as a matter of fact its much simpler with the new API

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ function locationFill() {
     var lng = location.coords.longitude;
     console.debug("Location permission granted");
     console.debug(lat + ", " + lng);
-    let url = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20180323&ll=" + lat + "," + lng + "&radius=16093&categoryId=4d4b7105d754a06374d81259&open_now=true&limit=60"
-    $.ajax({
-      dataType: "json",
-      url: url,
-      data: {},
-      success: function (data) {
-        console.debug(data);
-        console.debug("used - " + url)
-        var places = [data.response.venues[0].name, data.response.venues[1].name, data.response.venues[2].name, data.response.venues[3].name, data.response.venues[4].name, data.response.venues[5].name, data.response.venues[6].name, data.response.venues[7].name, data.response.venues[8].name, data.response.venues[9].name, data.response.venues[10].name, data.response.venues[11].name, data.response.venues[12].name, data.response.venues[13].name, data.response.venues[14].name, data.response.venues[15].name, data.response.venues[16].name, data.response.venues[17].name, data.response.venues[18].name, data.response.venues[19].name, data.response.venues[20].name, data.response.venues[21].name, data.response.venues[22].name, data.response.venues[23].name, data.response.venues[24].name, data.response.venues[25].name, data.response.venues[26].name, data.response.venues[27].name, data.response.venues[28].name, data.response.venues[29].name,];
-        var uniquePlaces = [];
-        for (let index = 0; index < places.length; index++) {
-          $.each(places, function (i, el) {
-            if ($.inArray(el, uniquePlaces) === -1) uniquePlaces.push(el);
-          })
-        }
-        console.debug(places);
-        console.debug(uniquePlaces);
-        for (let index = 0; index < uniquePlaces.length; index++) {
-          document.getElementById(index).innerHTML = uniquePlaces[index]
-        }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.debug(jqXHR);
-        console.debug(textStatus);
-        console.debug(errorThrown);
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://api.foursquare.com/v3/places/search?ll=" + lat + "," + lng + "&radius=16093&categories=13000&open_now=true&limit=32",
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: API_KEY
+      }
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      console.debug("used - " + settings.url)
+      var places = [response.results[0].name, response.results[1].name, response.results[2].name, response.results[3].name, response.results[4].name, response.results[5].name, response.results[6].name, response.results[7].name, response.results[8].name, response.results[9].name, response.results[10].name, response.results[11].name, response.results[12].name, response.results[13].name, response.results[14].name, response.results[15].name, response.results[16].name, response.results[17].name, response.results[18].name, response.results[19].name, response.results[20].name, response.results[21].name, response.results[22].name, response.results[23].name, response.results[24].name, response.results[25].name, response.results[26].name, response.results[27].name, response.results[28].name, response.results[29].name,];
+      var uniquePlaces = [];
+      for (let index = 0; index < places.length; index++) {
+        $.each(places, function (i, el) {
+          if ($.inArray(el, uniquePlaces) === -1) uniquePlaces.push(el);
+        })
+      }
+      console.debug(places);
+      console.debug(uniquePlaces);
+      for (let index = 0; index < uniquePlaces.length; index++) {
+        document.getElementById(index).innerHTML = uniquePlaces[index]
       }
     });
     const locationFillButton = document.getElementById("locationFillButton")
