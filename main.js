b17b395e5c39fe4cdf426a6e1947ee3ebb422710
Wyatt Nulton <legoman8304@gmail.com>
2023-01-26 00:24:41 -0500

    Its all coming together
    Added functionality to move venues from level one to level two when clicked by using buttons inside the <li> elements. L97-112 in main.js are prepping to delete the classes from level one once all of level two is filled.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function locationFill() {
     const settings = {
       async: true,
       crossDomain: true,
-      url: "https://api.foursquare.com/v3/places/search?ll=" + lat + "," + lng + "&radius=16093&categories=13000&open_now=true&limit=32",
+      url: "https://api.foursquare.com/v3/places/search?ll=" + lat + "," + lng + "&radius=16093&categories=13000&open_now=true&limit=50",
       method: "GET",
       headers: {
         accept: "application/json",
@@ -37,4 +37,76 @@ function locationFill() {
     function (error) {
       console.error("Location permission denied");
     });
+}
+function test() {
+  console.log("test")
+}
+function passWinnerToNextElement(winnerID) {
+  if (winnerID == 0 || winnerID == 1) {
+    console.debug("between 0 and 1")
+    document.getElementById("A0").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 2 || winnerID == 3) {
+    console.debug("between 2 and 3")
+    document.getElementById("A1").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 4 || winnerID == 5) {
+    console.debug("between 4 and 5")
+    document.getElementById("A2").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 6 || winnerID == 7) {
+    console.debug("between 6 and 7")
+    document.getElementById("A3").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 8 || winnerID == 9) {
+    console.debug("between 8 and 9")
+    document.getElementById("A4").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 10 || winnerID == 11) {
+    console.debug("between 10 and 11")
+    document.getElementById("A5").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 12 || winnerID == 13) {
+    console.debug("between 12 and 13")
+    document.getElementById("A6").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 14 || winnerID == 15) {
+    console.debug("between 14 and 15")
+    document.getElementById("A7").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 16 || winnerID == 17) {
+    console.debug("between 16 and 17")
+    document.getElementById("A8").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 18 || winnerID == 19) {
+    console.debug("between 18 and 19")
+    document.getElementById("A9").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 20 || winnerID == 21) {
+    console.debug("between 20 and 21")
+    document.getElementById("A10").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 22 || winnerID == 23) {
+    console.debug("between 22 and 23")
+    document.getElementById("A11").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 24 || winnerID == 25) {
+    console.debug("between 24 and 25")
+    document.getElementById("A12").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 26 || winnerID == 27) {
+    console.debug("between 26 and 27")
+    document.getElementById("A13").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 28 || winnerID == 29) {
+    console.debug("between 28 and 29")
+    document.getElementById("A14").innerHTML = document.getElementById(winnerID).textContent
+  } else if (winnerID == 30 || winnerID == 31) {
+    console.debug("between 30 and 31")
+    document.getElementById("A15").innerHTML = document.getElementById(winnerID).textContent
+  } else {
+    console.error("ERROR: ID of " + winnerID + " is out of range for pass")
+  }
+
+  if (document.getElementById("A0").innerHTML.trim().length > 0 && document.getElementById("A1").innerHTML.trim().length > 0 && document.getElementById("A2").innerHTML.trim().length > 0 && document.getElementById("A3").innerHTML.trim().length > 0 && document.getElementById("A4").innerHTML.trim().length > 0 && document.getElementById("A5").innerHTML.trim().length > 0 && document.getElementById("A6").innerHTML.trim().length > 0 && document.getElementById("A7").innerHTML.trim().length > 0) {
+    //run when all left side second level brackets have content
+    //use this to delete "current" class from level one, and add it to level two
+  }
+  if (document.getElementById("A8").innerHTML.trim().length > 0 && document.getElementById("A9").innerHTML.trim().length > 0 && document.getElementById("A10").innerHTML.trim().length > 0 && document.getElementById("A11").innerHTML.trim().length > 0 && document.getElementById("A12").innerHTML.trim().length > 0 && document.getElementById("A13").innerHTML.trim().length > 0 && document.getElementById("A14").innerHTML.trim().length > 0 && document.getElementById("A15").innerHTML.trim().length > 0) {
+    //run when all right side second level brackets have content
+    //KNOWN BUG: this method only works if the entire right side first level has been filled - which has yet to actually happen
+  }
+}
+function deleteClass(name) {
+  const element = document.getElementsByClassName(name);
+  console.debug("Deleted " + element.length + " element classes")
+  for (let i = 0; i < element.length; i + 1) {
+    element[i].classList.remove(name);
+  }
 }
